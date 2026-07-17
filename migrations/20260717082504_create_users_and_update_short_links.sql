37b61f9041337f691d3338fab8ea515f1d68abc1
Konstantin Pushkin <kostja10768@gmail.com>
2026-07-18 00:02:34 +0300

    fix: fix migrations 2

--- migrations/20260717082504_create_users_and_update_short_links.sql
+++ migrations/20260717082504_create_users_and_update_short_links.sql
@@ -1,34 +1,29 @@
 -- +goose Up
+create extension if not exists pgcrypto;
+
 create table users (
-    id bigserial primary key,
+    id         uuid primary key default gen_random_uuid(),
     first_name text not null,
-    last_name text not null,
-    email text not null,
+    last_name  text not null,
+    email      text not null,
     created_at timestamptz not null default now()
 );
 
 create unique index on users (lower(email));
 
 create table short_links (
-    id bigserial primary key,
+    id           uuid primary key default gen_random_uuid(),
     original_url text not null,
-    short_code text not null unique,
-    created_at timestamptz not null default now(),
-    user_id bigint not null references users(id) on delete restrict,
-    click_count bigint not null default 0,
-    expires_at timestamptz
+    short_code   text not null unique,
+    created_at   timestamptz not null default now(),
+    user_id      uuid not null references users(id) on delete restrict,
+    click_count  bigint not null default 0,
+    expires_at   timestamptz
 );
 
 create index on short_links(user_id);
 create index on short_links(expires_at) where expires_at is not null;
 
 -- +goose Down
-CREATE TABLE links (
-    id           BIGSERIAL PRIMARY KEY,
-    short_code   VARCHAR(16) NOT NULL UNIQUE,
-    original_url TEXT NOT NULL,
-    created_at   TIMESTAMPTZ NOT NULL DEFAULT NOW()
-);
-
-DROP TABLE users;
-DROP TABLE short_links;
+drop table if exists short_links;
+drop table if exists users;
